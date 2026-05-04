c004c5a7f694da4726e14963566c72ea4c940134
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2026-05-04 15:50:54 -0300

    operadores aritimético

---
+++ operadores.dart
@@ -0,0 +1,15 @@
+void main() {
+  double num1 = 10;
+  double num2 = 15;
+  double resultado = num1 + num2;
+  print('O resultado da soma é $resultado');
+
+  resultado = num2 - num1;
+  print('O resultado da subtração é $resultado');
+
+  resultado = 10 / 2;
+  print('O resultado da divisão é $resultado');
+
+  resultado = 30 * 10;
+  print('O resultado da multiplicação é $resultado');
+}
