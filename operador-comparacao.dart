a42834ed2f6188415ad12874790ffd385f7b047f
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2026-05-05 15:48:28 -0300

    operador de comparação

---
+++ operador-comparacao.dart
@@ -0,0 +1,19 @@
+void main() {
+  double modulo = 10 % 3;
+  print("O resultado restante da divisão é $modulo");
+
+  bool situacao = 10 > 5;
+  print(situacao);
+
+  situacao = 10 < 5;
+  print(situacao);
+
+  situacao = 10 >= 5;
+  print(situacao);
+
+  situacao = 10 == 10;
+  print(situacao);
+
+  situacao = 10 != 10;
+  print(situacao);
+}
