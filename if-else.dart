86a9e395fa998a87ef2326e51c2b274f77c1eef0
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2026-05-20 10:09:10 -0300

    if else

---
+++ if-else.dart
@@ -0,0 +1,9 @@
+void main() {
+  double salario = 2000.50;
+
+  if (salario >= 1621) {
+    print("Você recebe um salário bacana... ");
+  } else {
+    print('Você não recebe uma salário! Que pena!!!');
+  }
+}
