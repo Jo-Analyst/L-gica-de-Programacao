04c547a01d03c5fd62e7248428ad8eda6105f7d3
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2026-06-02 15:49:10 -0300

    Versionamento: atualiza e adiciona arquivos locais

--- tabuada-while.dart
+++ tabuada-while.dart
@@ -1,31 +1,13 @@
-// import 'dart:io';
-
-// void main() {
-//   stdout.write('Digite o número para a tabuada: ');
-//   int numero = int.parse(stdin.readLineSync()!);
-//   int i = 1;
-
-//   while (i <= 10) {
-//     print('$numero x $i = ${numero * i}');
-//     i++;
-//   }
-// }
-
 import 'dart:io';
 
 void main() {
-  stdout.write('Até que número será realizado a tabuada? ');
-  int numero =  int.parse(stdin.readLineSync()!);
-  int i = 1;
-  int j = 1;
+  stdout.write('Digite o valor para a tabuada: ');
+  int valor = int.parse(stdin.readLineSync()!);
+  int numero = 1;
 
-  while (i <= numero) {
-    j = 1;
-    print('tabuada $i');
-    while (j <= 10) {
-      print('$i x $j = ${i * j}');
-      j++;
-    }
-    i++;
+  while (numero <= 10) {
+    int resultado = valor * numero;
+    print('$valor x $numero = $resultado');
+    numero++;
   }
 }
---
+++ while.dart
@@ -0,0 +1,8 @@
+void main() {
+  int numero = 100;
+
+  while (numero > 0) {
+    print('Número $numero');
+    numero--;
+  }
+}
